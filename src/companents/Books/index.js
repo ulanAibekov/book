24f65469a896f22a9abe224e65934d47c4493172
ulandevv <usopuev8@gmail.com>
2023-06-20 21:06:43 +0300

    books

--- src/companents/Books/index.js
+++ src/companents/Books/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import book1 from '../../img/book-1.png'
 
 const Books = () => {
     return (
-        <div>
-            <h1>Ulan</h1>
+        <div id='books'>
+            <div className="container">
+                <div className="books">
+                    <div className="books--title">
+                        <h1>Books</h1>
+                        <h3>View all</h3>
+                    </div>
+                    <div className="books--block">
+                        <div className="book">
+                            <img src={book1} alt="img"/>
+                            <h2>THE SUBTLE ART OF <br/> NOT GIVING A F*CK</h2>
+                            <p>by Mark Manson</p>
+                        </div>
+                        <div className="book">
+                            <img src={book1} alt="img"/>
+                            <h2>THE SUBTLE ART OF <br/> NOT GIVING A F*CK</h2>
+                            <p>by Mark Manson</p>
+                        </div>
+                        <div className="book">
+                            <img src={book1} alt="img"/>
+                            <h2>THE SUBTLE ART OF <br/> NOT GIVING A F*CK</h2>
+                            <p>by Mark Manson</p>
+                        </div>
+                        <div className="book">
+                            <img src={book1} alt="img"/>
+                            <h2>THE SUBTLE ART OF <br/> NOT GIVING A F*CK</h2>
+                            <p>by Mark Manson</p>
+                        </div>
+                        <div className="book">
+                            <img src={book1} alt="img"/>
+                            <h2>THE SUBTLE ART OF <br/> NOT GIVING A F*CK</h2>
+                            <p>by Mark Manson</p>
+                        </div>
+                        <div className="book">
+                            <img src={book1} alt="img"/>
+                            <h2>THE SUBTLE ART OF <br/> NOT GIVING A F*CK</h2>
+                            <p>by Mark Manson</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
